cecfa02e8f6af405030ef275278ea6911b1f4fe7
Regina Lio <regina_lio@hotmail.com>
2014-12-28 02:10:36 +0000

    FIXED bug in divideToPatches2, optimisePatchSize now plot quality graph for LcR

--- Matlab/combinePatches.m
+++ Matlab/combinePatches.m
@@ -9,7 +9,7 @@ function x = combinePatches(xHR, HR_OVERLAP)
         start1 = size(xHR,1) - HR_OVERLAP + 1;
         start2 = start1+HR_OVERLAP;
         v = [];
-        for i = 1:size(xHR,3)-1
+        for i = 1:size(xHR,3)-2
             x(start1:start2-1, :) = 0.5*(x(start1:start2-1, :)+x(start2:start2+HR_OVERLAP-1, :));
             x(:, start1:start2-1) = 0.5*(x(:, start1:start2-1)+x(:, start2:start2+HR_OVERLAP-1));
             v = cat(1,v,start2:start2+HR_OVERLAP-1);
@@ -17,6 +17,15 @@ function x = combinePatches(xHR, HR_OVERLAP)
             start2 = start2 + size(xHR,1);
         end
         
+        % width of last overlap space (16*2 + 20) - (60-20) = 12
+        lastOverlap = ((size(xHR,1)-HR_OVERLAP)*(size(xHR,3)-2)+size(xHR,1)) - (60 - size(xHR,1));
+        
+        % average last overlapping column/row
+        start1 = width - size(xHR,1) - lastOverlap + 1;
+        start2 = width - size(xHR,1) + 1;
+        x(start1:start2-1, :) = 0.5*(x(start1:start2-1, :)+x(start2:start2+lastOverlap-1, :));
+        x(:, start1:start2-1) = 0.5*(x(:, start1:start2-1)+x(:, start2:start2+lastOverlap-1));
+        v1 = [start2:start2+lastOverlap-1];
        
 %         start1 = size(xHR,1) - HR_OVERLAP/2;
 %         endIndex = start1 + HR_OVERLAP;
@@ -30,6 +39,9 @@ function x = combinePatches(xHR, HR_OVERLAP)
 %         end
         
         % remove extra rows and columns
+        x(v1,:) = [];    
+        x(:,v1) = [];
         x(v,:) = [];    
         x(:,v) = [];
+
 end
---
+++ Matlab/optimisePatchSize.m
@@ -0,0 +1,106 @@
+function optimisePatchSize
+% find result quality for each patchWidth to determine optimial patch width
+
+
+    load('parameters.mat');
+    patchWidth = [2, 3, 4, 5];
+    
+    SSIMLcR = zeros(1, length(patchWidth));
+    SSIMBI = zeros(1, length(patchWidth));
+    
+    pSNRLcR = zeros(1, length(patchWidth));
+    pSNRBI = zeros(1, length(patchWidth));
+    
+    % get training and testing data
+    data = readDataSet(922);    
+    trainSize = 360;
+    trainRange = [1, 1674];
+    testSize = 40;
+    testRange = [1675, 1846];
+
+    trainSet.groundTruth = data(:, :, randperm(trainRange(2), trainSize));
+    testSet.groundTruth = data(:,:,randperm(diff(testRange)+1, testSize)+trainRange(2));
+    
+    % determine result quality for each patchWidth
+    for i = 1:length(patchWidth)
+        disp(strcat('patch width:', num2str(patchWidth(i))));
+        parameters.LRPatch = patchWidth(i);
+        parameters.HRPatch = patchWidth(i)*parameters.ratio;
+        
+        disp('getting train and test data...');
+        %%% downsample to LR and HR
+        trainSet.LR = imresize(trainSet.groundTruth, parameters.LRSize);
+        trainSet.HR = imresize(trainSet.groundTruth, parameters.HRSize);
+        
+        testSet.LR = imresize(testSet.groundTruth, parameters.LRSize);
+        testSet.HR = imresize(testSet.groundTruth, parameters.HRSize);
+        
+        %%% obtain patched version
+        [trainSet.LR_p, trainSet.HR_p] = divideToPatches2(trainSet.LR, trainSet.HR, parameters);
+        [testSet.LR_p, testSet.HR_p] = divideToPatches2(testSet.LR, testSet.HR, parameters);
+        
+        %%% reconstruction via LcR
+        resultLcR = reconstruction(testSet.LR_p,trainSet.LR_p, trainSet.HR_p,...
+                   parameters.tau, parameters.HROverlap);
+        resultBI = bicubicInterpolation(testSet.LR);
+
+        [widthBI, ~, N] = size(resultBI);
+        testSet.HR_BI = imresize(data(:,:,1:N), [widthBI, widthBI]);
+
+        %%% measure output quality
+        SSIMLcR(i) = averageSSIM(resultLcR, testSet.HR);
+        SSIMBI(i) = averageSSIM(resultBI, testSet.HR_BI);
+
+        pSNRLcR(i) = averagePSNR(resultLcR, testSet.HR);
+        pSNRBI(i) = averagePSNR(resultBI, testSet.HR_BI);
+        
+        
+    end
+    
+    figure;
+    plot(patchWidth, SSIMLcR);
+end
+
+function [patchedLR, patchedHR] = divideToPatches2(LR, HR, params)
+% divide dataHR/LR to patches -> yHR/LR
+    
+    % End patch index for each face image
+    U = ceil((params.HRSize(1)-params.HROverlap)/(params.HRPatch-params.HROverlap));
+    V = U;
+
+    N = size(HR,3);
+    stepHR = floor(params.HRSize(1)/U);
+    stepLR = floor(params.LRSize(1)/U);
+
+    patchedHR = zeros(params.HRPatch, params.HRPatch, U,V,N, 'uint8');
+    patchedLR = zeros(params.LRPatch, params.LRPatch, U,V,N, 'uint8');
+
+   
+    for k = 1:N
+        for i = 1:U
+            for j = 1:V
+                rectHR = [(1+(i-1)*stepHR) (1+(j-1)*stepHR) params.HRPatch-1 params.HRPatch-1];
+                rectLR = [(1+(i-1)*stepLR) (1+(j-1)*stepLR) params.LRPatch-1 params.LRPatch-1];
+
+                % check for last column and last row of patches
+                if (rectHR(1)+rectHR(3)) > params.HRSize
+                    rectHR(1) = params.HRSize(1) - params.HRPatch + 1;
+                end
+                if (rectHR(2)+rectHR(4)) > params.HRSize
+                    rectHR(2) = params.HRSize(2) - params.HRPatch + 1;
+                end
+                
+                if (rectLR(1)+rectLR(3)) > params.LRSize
+                    rectLR(1) = params.LRSize(1) - params.LRPatch + 1;
+                end
+                if (rectLR(2)+rectLR(4)) > params.LRSize
+                    rectLR(2) = params.LRSize(2) - params.LRPatch + 1;
+                end
+                
+                patchedHR(:,:,i,j,k) = imcrop(HR(:,:,k), rectHR);
+                patchedLR(:,:,i,j,k) = imcrop(LR(:,:,k), rectLR);
+            end
+        end
+    end
+
+end
